aab9e5b7684bdaee5285bb719f12a65b6c2346b4
Sheetal prajapati <68459680+sheetal4532@users.noreply.github.com>
2025-10-18 19:33:32 +0530

    Update app.js

--- sms/app.js
+++ sms/app.js
@@ -1,41 +1,9 @@
-// const express = require('express')
-// const twilio = require('twilio')
-// const dotenv = require('dotenv')
-// const app = express()
-
-//  dotenv.config()
-
-//  function sendSMS(){
-
-//  const client = new twilio(process.env.TWILIO_SID ,process.env.TWILIO_AUTH_TOKEN)
-
-//  return  client.messages
-//  .create({body:'hey ur delivery has been delivered' , from:'+918223927220' , to: process.env.PHONE_NUMBER})
-//  .then(message => {
-    
-//         console.log(message , "Message sent")
-
-// })
-//  .catch(err =>  {
-    
-//     console.log(err , "Message not sent")
-
-// })
-
-// }
-
-// sendSMS()
-
-// app.listen(5000 , () => console.log('listening at port 5000'))
-
 // Twilio credentials
-const accountSid = 'AC2e70d9b7533eadab010f5a9093bb6790';
-const authToken = '86fc113ff8bdeb120f03ed9b826ade15';
+const accountSid = '';
+const authToken = '';
 const twilioPhoneNumber = '+13158738257'; // Replace with your Twilio phone number
-
 // Required dependencies
 const twilio = require('twilio')(accountSid, authToken);
-
 // Function to send an SMS
 function sendSMS(toPhoneNumber, message) {
   twilio.messages
@@ -47,9 +15,7 @@ function sendSMS(toPhoneNumber, message) {
     .then(message => console.log('SMS sent successfully. SID:', message.sid))
     .catch(error => console.error('Error sending SMS:', error));
 }
-
 // Usage example
-const recipientPhoneNumber = '+918223927220'; // Replace with the recipient's phone number
+const recipientPhoneNumber = '+91....'; // Replace with the recipient's phone number
 const smsMessage = 'Thanks for shopping with veggies shopping ! We would love to hear about your product quality experience ';
-
 sendSMS(recipientPhoneNumber, smsMessage);
