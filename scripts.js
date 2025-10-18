9d9beaccf32e8ba7b941741d9ffb8b030bf8b9f4
Sheetal prajapati <68459680+sheetal4532@users.noreply.github.com>
2025-10-18 18:19:49 +0530

    Refactor scripts.js for cleaner code
    Removed unnecessary whitespace and improved formatting.

--- scripts.js
+++ scripts.js
@@ -1,55 +1,39 @@
 let searchForm = document.querySelector('.search-form');
-
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
-
 let shoppingCart = document.querySelector('.shopping_cart');
-
 document.querySelector('#cart-btn').onclick = () => {
     shoppingCart.classList.toggle('active');
     searchForm.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
-
 let loginForm = document.querySelector('.login-form');
-
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     navbar.classList.remove('active');
 }
-
-
-
-
 let navbar = document.querySelector('.navbar');
-
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
-
 }
-
-
 window.onscroll = () => {
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
-
 let countDate = new Date('July 1, 2026 00:00:00').getTime();
-
 function countDown() {
-
     let now = new Date().getTime();
     gap = countDate - now;
 
@@ -67,13 +51,10 @@ function countDown() {
     document.getElementById('hour').innerText = h;
     document.getElementById('minute').innerText = m;
     document.getElementById('second').innerText = s;
-
 }
-
 setInterval(function() {
     countDown();
 }, 1000)
-
 var Swiper = new Swiper(".review-slider", {
     speed: 1000,
     loop: true,
